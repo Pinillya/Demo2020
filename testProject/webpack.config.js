369cf3ab70704c47f9c90f3ef343050cbf44e4d9
Illy <ida@ravn.no>
2019-08-01 13:13:56 +0200

    webpack and npm established

---
+++ testProject/webpack.config.js
@@ -0,0 +1,23 @@
+ const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: "html-loader",
+                    options: {minimize: true}
+                }
+                ]
+            }
+        ]
+    },
+    plugins: [
+         new HtmlWebPackPlugin({
+             template: "./src/index.html",
+             filename: "./index.html"
+         })
+    ]
+}
